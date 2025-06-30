7d41f202c9d293a71cec53d8c2f345161821deba
uttamkant <uttamkalindi07@gmail.com>
2025-06-30 03:45:02 -0700

    third commit

--- Admin_area/index.php
+++ Admin_area/index.php
@@ -9,6 +9,7 @@
     <link rel="stylesheet" href="../style/all.min.css">
     <!--css file--> 
   <link rel="stylesheet" href="../style/style.css">
+ 
 </head>
 <body>
     
@@ -29,7 +30,30 @@
     </nav>
     <!--second child--> 
     <div class="bg-light">
-        <h3 class="text-center p-2">Mange <Details></h3>
+        <h3 class= "text-center p-2">Mange Details</h3>
+    </div>
+
+    <!--third child-->
+    <div class="row">
+        <div class="col-md-12 bg-secondary p-1 d-flex alight-itmes-center">
+            <div class="px-30">
+                <a href="#"><img src="../images/logo.png"  
+                alt="" class="admin_image"></a>
+                <p class="text-light-center">Admin Name</p>
+            </div>
+<div class="button text-center my-30 mx-30 py-30 px-30">
+    <button class="my-30 mx-30 py-30 px-30 bg-info"><a href="" class="nav-link text-light bg-info my-1">Insert Product</a></button>
+    <button><a href="" class="nav-link text-light bg-info my-1">View Product</a></button>
+    <button><a href="" class="nav-link text-light bg info my-1">Insert Category</a></button>
+    <button><a href="" class="nav-link text-light bg info my-1">Veiw Category</a></button>
+    <button><a href="" class="nav-link text-light bg info my-1">Insert Brands</a></button>
+    <button><a href="" class="nav-link text-light bg info my-1">All orders</a></button>
+    <button><a href="" class="nav-link text-light bg info my-1">All payments</a></button>
+    <button><a href="" class="nav-link text-light bg info my-1">List user</a></button>
+    <button><a href="" class="nav-link text-light bg info my-1">View brands</a></button>
+    <button><a href="" class="nav-link text-light bg info my-1">Logout</a></button>
+</div>
+        </div>
     </div>
 </div>
 
