b4ffce7ec112b94f4afc29163515bed3ad89ede3
uttamkant <uttamkalindi07@gmail.com>
2025-07-08 02:41:21 -0700

    12th commit

--- Admin_area/functions/common_function.php
+++ Admin_area/functions/common_function.php
@@ -1,9 +1,9 @@
 <?php
 
 // including connect file
-include('./includes/connect.php');
+include('../includes/connect.php');
 
-// getting  product
+// getting  products
 function getproducts(){
     global $conn;
     $brand_id_query="Select *from `products` order by rand() LIMIT 0,9";
@@ -83,14 +83,152 @@ echo "<li class='nav-item'>
 // getting unique categories
 function get_uniqe_brand(){
     global $conn;
-if(!isset($_GET['brand'])){
-    $result_query="Select *from `products` where brand_id=$brand_id";
-    $result_query=mysqli_query($conn,$select_query);{
-if($num_of_row==00){
-    echo "<h2 class='text-center text-denger'> this is not  avaialbe for servies</h2>";
+    if(!isset($_GET['brand'])){
+        $result_query="Select *from `products` where brand_id=$brand_id";
+        $result_query=mysqli_query($conn,$select_query);
+        if($num_of_row==00){
+            echo "<h2 class='text-center text-denger'> this is not  avaialbe for servies</h2>";
+
+        }
+    }
+
+    // $row=mysqli_fetch_assoc($result_query);
+    // echpo $row['product_title'];
+    while($row=mysqli_fetch_assoc($result_query)){
+        $product_id=$row['product_id'];
+        $product_title=$row['product_title'];
+        $product_description=$row['product_description'];
+        $product_image1=$row['product_iimage1'];
+        $product_price=$row['product_price'];
+        $category_id=$row['category_id'];
+        $brand_id=$row['brands_id'];
+        echo "<div class='col-md-4mb-2'>
+                <div class='card'>
+                    <img src='./admin_area/product_images/$product_image1'
+                        class='card-img-top' alt='$product_title'>
+                    <div class='card-body'>
+                        <h5 class='card-title'>$product_title</h5>
+                        <p class='card-text'>$product_description</p>
+                        <a href='#' ='class btn btn-info'> Add to cart</a>
+                        <a href='product_details.php?product_id=$product_id'
+                            class='btn btn-seconday'>View more</a>
+                    </div>
+                </div>
+               </div>";
+    }
+    
+}  
+
+
+//getting all products
+
+function get_all_products(){
+    
+    global $conn;
+
+
+    // condition to check isset or not 
+    if(isset($_GET['category'])){
+        $category_id=$_GET['category'];
+        $result_query="Select *from `products` where category_id=$category_id";
+        $result_query=mysqli_query($conn,$select_query);
+        $num_of_rows=mysqli_num_rows($result_query);
+        if($num_of_row==0){
+            echo "<h2 class='text-center text-denger'>No stock for this category</h2>";
+
+        }
+
+
+    // $row=mysqli_fetch_assoc($result_query);
+    // echpo $row['product_title'];
+    while($row=mysqli_fetch_assoc($result_query)){
+        $product_id=$row['product_id'];
+        $product_title=$row['product_title'];
+        $product_description=$row['product_description'];
+        $product_image1=$row['product_image1'];
+        $product_price=$row['product_price'];
+        $category_id=$row['category_id'];
+        $brand_id=$row['brands_id'];
+   echo "<div class='col-md-4 mb-2'>
+        <div class='card'>
+               <img src='./admin_area/product_images/$product_image1'
+               class='card-img-top' alt='$product_title'>
+               <div class='card-body'>
+               <h5 class='card-title'>$product_title</h5>
+               <p class='card-text'>$product_description</p>
+               <a href='#' ='class btn btn-ifo'> Add to cart</a>
+               <a href='#' class='btn btn-seconday'>View more</a>
+               </div>
+               </div>
+               </div>";
+
 
 }
     }
+}
+
+
+}
+
+
+        // getting unique categories
+
+function get_uniqe_brands(){
+    global $conn;
+if(isset($_GET['brands'])){
+    $brands_id=$_GET['brands_id'];
+    $brand_id_query="Select *from `brands` order by rand() LIMIT 0,9";
+    $result_query=mysqli_query($conn,$select_query);{
+    // $row=mysqli_fetch_assoc($result_query);
+    // echpo $row['product_title'];
+    while($row=mysqli_fetch_assoc($result_query)){
+    $product_id=$row['product_id'];
+    $product_title=$row['product_title'];
+    $product_description=$row['product_description'];
+    $product_image1=$row['product_iimage1'];
+    $product_price=$row['product_price'];
+    $category_id=$row['category_id'];
+    $brand_id=$row['brands_id'];
+   echo "<div class='col-md-4mb-2'>
+            <div class='card'>
+               <img src='./admin_area/product_images/$product_image1'
+               class='card-img-top' alt='$product_title'>
+               <div class='card-body'>
+                    <h5 class='card-title'>$product_title</h5>
+                    <p class='card-text'>$product_description</p>
+                    <a href='#' class='btn btn-info'> Add to cart</a>
+                    <a href='#' class='btn btn-seconday'>View more</a>
+                </div>
+            </div>
+        </div>";
+
+
+}
+    }
+}
+
+}
+
+
+
+
+
+
+
+//searching product funnction
+
+function search_products(){
+    global $conn;
+    if(isser($_GET['search_data_product'])){
+        $search_data_value=$_GET['search_data'];
+    $search_query="Select * from `product` where product_keywords like 'search_data_value'";
+    $result_query=mysqli_query($conn,$result_query);
+$num_of_rows=mysqli_num_rows($result_query);
+if($num_of_row==0){
+    echo "<h2 class='text-center text-denger'>No stock for this category</h2>";
+
+}
+
 
     // $row=mysqli_fetch_assoc($result_query);
     // echpo $row['product_title'];
@@ -109,145 +247,11 @@ if($num_of_row==00){
                <div class='card-body'>
                <h5 class='card-title'>$product_title</h5>
                <p class='card-text'>$product_description</p>
-               <a href='#' ='class btn btn-info'> Add to cart</a>
-               <a href='product_details.php?product_id=$product_id'
-               class='btn btn-seconday'>View more</a>
+               <a href='' class='btn btn-info'> Add to cart</a>
+               <a href='#' class='btn btn-seconday'>View more</a>
                </div>
                </div>
-               </div>;
-
-
-}
-    
-
-
-//getting all products
-function_get_all_products(){
-   global $conn;
-
-
-    // condition to check isset or not 
-    if(isset($_GET['category'])){
-        $category_id=$_GET['category'];
-    $result_query="Select *from `products` where category_id=$();
-    $result_query=mysqli_query($conn,$select_query);
-$num_of_rows=mysqli_num_rows($result_query);
-if($num_of_row==00){
-    echo "<h2 class='text-center text-denger'>No stock for this category</h2>";
-
-}
-
-
-    // $row=mysqli_fetch_assoc($result_query);
-    // echpo $row['product_title'];
-    while($row=mysqli_fetch_assoc($result_query));{
-    $product_id=$row['product_id'];
-    $product_title=$row['product_title'];
-    $product_description=$row['product_description'];
-    $product_image1=$row['product_iimage1'];
-    $product_price=$row['product_price'];
-    $category_id=$row['category_id'];
-    $brand_id=$row['brands_id'];
-   echo "<div class='col-md-4mb-2'>"
-   <div class='card'>
-               <img src='./admin_area/product_images/$product_image1'
-               class='card-img-top' alt='$product_title'>
-               <div class="card-body">
-               <h5 class='card-title'>$product_title</h5>
-               <p class="card-text">$product_description</p>
-               <a href="" ="class btn btn-nfo"> Add to cart</a>
-               <a href="#" class="btn btn-seconday">View more</a>
-               </div>
-               </div>
-               </div>;
-
-
-}
-    }
-}
-
-
-}
-
-
-// getting unique categories
-function get_uniqe_brand(){
-    global $conn;
-if(!isset($_GET['brand_id'])){
-    $brand_id_query="Select *from `brands` order by rand() LIMIT 0,9";
-    $result_query=mysqli_query($conn,$select_query);{
-    // $row=mysqli_fetch_assoc($result_query);
-    // echpo $row['product_title'];
-    while($row=mysqli_fetch_assoc($result_query));{
-    $product_id=$row['product_id'];
-    $product_title=$row['product_title'];
-    $product_description=$row['product_description'];
-    $product_image1=$row['product_iimage1'];
-    $product_price=$row['product_price'];
-    $category_id=$row['category_id'];
-    $brand_id=$row['brands_id'];
-   echo "<div class='col-md-4mb-2'>"
-   <div class='card'>
-               <img src='./admin_area/product_images/$product_image1'
-               class='card-img-top' alt='$product_title'>
-               <div class="card-body">
-               <h5 class='card-title'>$product_title</h5>
-               <p class="card-text">$product_description</p>
-               <a href="" ="class btn btn-nfo"> Add to cart</a>
-               <a href="#" class="btn btn-seconday">View more</a>
-               </div>
-               </div>
-               </div>;
-
-
-}
-    }
-}
-}
-
-
-
-
-
-
-
-//searching product funnction
-
-function search_products(){
-    global $conn;
-    if(isser($_GET['search_data_product'])){
-        $search_data_value=$_GET['search_data'];
-    $search_query="Select * from `product` where product_keywords like 'search_data_value'"
-    $result_query=mysqli_query($conn,$result_query);
-$num_of_rows=mysqli_num_rows($result_query);
-if($num_of_row==00){
-    echo "<h2 class='text-center text-denger'>No stock for this category</h2>";
-
-}
-
-
-    // $row=mysqli_fetch_assoc($result_query);
-    // echpo $row['product_title'];
-    while($row=mysqli_fetch_assoc($result_query));{
-    $product_id=$row['product_id'];
-    $product_title=$row['product_title'];
-    $product_description=$row['product_description'];
-    $product_image1=$row['product_iimage1'];
-    $product_price=$row['product_price'];
-    $category_id=$row['category_id'];
-    $brand_id=$row['brands_id'];
-   echo "<div class='col-md-4mb-2'>"
-   <div class='card'>
-               <img src='./admin_area/product_images/$product_image1'
-               class='card-img-top' alt='$product_title'>
-               <div class="card-body">
-               <h5 class='card-title'>$product_title</h5>
-               <p class="card-text">$product_description</p>
-               <a href="" ="class btn btn-nfo"> Add to cart</a>
-               <a href="#" class="btn btn-seconday">View more</a>
-               </div>
-               </div>
-               </div>;
+               </div>";
 
 
 }
@@ -256,23 +260,32 @@ if($num_of_row==00){
 
 //view details funtion
 function view_details(){
-   ow=(mysqli_fetch_assoc($result_query));{
+  
+    global $conn;
+    $brand_id_query="Select *from `products` order by rand() LIMIT 0,9";
+    $result_query=mysqli_query($conn,$select_query);
+    while($row=mysqli_fetch_assoc($result_query)){
     $product_id=$row['product_id'];
     $product_title=$row['product_title'];
     $product_description=$row['product_description'];
-    $product_image1=$row['product_iimage1'];
+    $product_image1=$row['product_image1'];
     $product_price=$row['product_price'];
     $category_id=$row['category_id'];
     $brand_id=$row['brands_id'];
-   echo "<div class='col-md-4mb-2'>"
+   echo "<div class='col-md-4mb-2'>                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
    <div class='card'>
-               <img src='./admin_area/product_images/$product_image1'
+               <img src='./Admin_area/product_images/$product_image1'
                class='card-img-top' alt='$product_title'>
-               <div class="card-body">
+               <div class='card-body'>
                <h5 class='card-title'>$product_title</h5>
-                global $conn;
+              <p class='card-text'>$product_description</p>
+              <a href='#' class='btn btn-info'>Add to cart</a>
+           <a href='#' class='btn btn-secondary'>View more</a>
+           </div>
 
-
+        </div>
+        </div>";
+}
     // condition to check isset or not 
     if(isset($_GET['product_id'])){
     if(!isset($_GET['category'])){
@@ -316,15 +329,15 @@ if($num_of_row==00){
     }
 }  
 }
-}
+
 
 
 
 //getip address function();
-dunction getIPAddress(){
+/*function getIPAddress(){
     //whether ip is from the share internet
     id(!empty($_Server['HTTP_CLINT_IP'])){
-        $ip = $SERVER['HTTP_CLINENT_IP'];
+        $ip = $_SERVER['HTTP_CLINENT_IP'];
     }
     //where ip is from the proxy
     elesif (!empty($_SERVER['HTTP_X_FORWARD_FOR'])){
@@ -343,6 +356,6 @@ $ip = $_SERVER['HTTP_X_FORWARED_FOR'];
 // cart function
 function cart(){
 
-}
+}*/
 ?>
 
